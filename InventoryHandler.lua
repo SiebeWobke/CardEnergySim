5375d860e5e1be9665a729225723ec364febab5a
siebewobke <devries_jouke@hotmail.com>
2024-05-24 21:09:33 +0200

    Add or update exported scripts

--- InventoryHandler.lua
+++ InventoryHandler.lua
@@ -1,28 +1,28 @@
 local replicatedStorage = game:GetService("ReplicatedStorage")
 local cardPackOpenedEvent = replicatedStorage:WaitForChild("CardPackOpenedEvent")
 
-local function addPetToInventory(player, petName)
+local function addMultiplePetsToInventory(player, petNames)
 	if not player:FindFirstChild("Inventory") then
 		local inventory = Instance.new("Folder", player)
 		inventory.Name = "Inventory"
 	end
 
 	local inventory = player:FindFirstChild("Inventory")
-	local existingPet = inventory:FindFirstChild(petName)
 
-	if existingPet then
-		existingPet.Value = existingPet.Value + 1
-	else
-		local pet = Instance.new("IntValue", inventory)
-		pet.Name = petName
-		pet.Value = 1
+	for _, petName in pairs(petNames) do
+		local existingPet = inventory:FindFirstChild(petName)
+		if existingPet then
+			existingPet.Value = existingPet.Value + 1
+		else
+			local pet = Instance.new("IntValue", inventory)
+			pet.Name = petName
+			pet.Value = 1
+		end
 	end
-
-	cardPackOpenedEvent:FireClient(player, petName)  -- Notify the client to update the UI
 end
 
-cardPackOpenedEvent.OnServerEvent:Connect(function(player, petName)
-	if petName then
-		addPetToInventory(player, petName)
+cardPackOpenedEvent.OnServerEvent:Connect(function(player, petNames)
+	if petNames and type(petNames) == "table" then
+		addMultiplePetsToInventory(player, petNames)
 	end
 end)
