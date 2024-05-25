e311f1d87af58dda85be9c961c8c809c1f8a2aa0
siebewobke <devries_jouke@hotmail.com>
2024-05-25 22:28:38 +0200

    Add or update exported scripts

--- RebirthHandler.lua
+++ RebirthHandler.lua
@@ -3,7 +3,7 @@ local rebirthEvent = replicatedStorage:WaitForChild("RebirthEvent")
 
 local rebirthRequirements = {
 	10, 125, 187.5, 375, 1125, 5625, 7031.25, 10546.875, 21093.75, 63281.25,
-	316406.25, 395507.8125, 593261.71875, 1186523.4375, 3559570.3125, 17797851.5625,
+	316406.25, 395507.8125, 1186523.4375, 3559570.3125, 17797851.5625,
 	22247314.453125, 33370971.6796875, 66741943.359375, 200225830.078125,
 	1001129150.390625, 1251411437.9882812, 1877117156.9824219, 3754234313.9648438, 
 	11262702941.894531, 56313514709.472656, 70391893386.84082, 105587840080.26123,
@@ -12,18 +12,22 @@ local rebirthRequirements = {
 }
 
 local function setPlayerMultiplierAndLuck(player, value)
+	local multiplier = player:FindFirstChild("Multiplier")
 	local leaderstats = player:FindFirstChild("leaderstats")
-	if leaderstats then
-		local multiplier = leaderstats:FindFirstChild("Multiplier")
-		local luck = leaderstats:FindFirstChild("Luck")
-		if multiplier then
-			multiplier.Value = value
-			print("Updated Multiplier: " .. multiplier.Value)  -- Debugging line
-		end
-		if luck then
-			luck.Value = value
-			print("Updated Luck: " .. luck.Value)  -- Debugging line
-		end
+	if multiplier then
+		multiplier.Value = value
+		print("Updated Multiplier: " .. multiplier.Value)  -- Debugging line
+	else
+		multiplier = Instance.new("IntValue")
+		multiplier.Name = "Multiplier"
+		multiplier.Value = value
+		multiplier.Parent = player
+		print("Created and updated Multiplier: " .. multiplier.Value)  -- Debugging line
+	end
+	local luck = leaderstats:FindFirstChild("Luck")
+	if luck then
+		luck.Value = value
+		print("Updated Luck: " .. luck.Value)  -- Debugging line
 	end
 end
 
